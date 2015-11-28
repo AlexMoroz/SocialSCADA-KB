5bba8c19f81c2959c582098f5c69f14602557389
Bastian Rodehüser <bastian.rodehueser@tum.de>
2015-11-28 19:40:28 +0100

    worked on toDoListTemplates

--- public/controllers/createToDoListTemplatesCtrl.js
+++ public/controllers/createToDoListTemplatesCtrl.js
@@ -1,4 +1,4 @@
-app.controller('createToDoListTemplatesCtrl', function($scope, $http, $location) {
+app.controller('createToDoListTemplatesCtrl', function($scope, $http, $location, $mdDialog, $mdMedia) {
     $scope.submit = function() {
         $http.post('/createToDoListTemplatesCtrl', $scope.formData)
             .success(function(data) {
@@ -15,5 +15,41 @@ app.controller('createToDoListTemplatesCtrl', function($scope, $http, $location)
             .error(function(data) {
                 console.log('Error: ' + data);
             });
-    }
+    };
+    $scope.status = '  ';
+    $scope.customFullscreen = $mdMedia('sm');
+    $scope.showAdvanced = function(ev) {
+        $mdDialog.show({
+                controller: DialogController,
+                templateUrl: 'dialog.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose:false
+            });
+        $scope.$watch(function() {
+            return $mdMedia('sm');
+        }, function(sm) {
+            $scope.customFullscreen = (sm === true);
+        });
+    };
+
+    this.toDoListTemplate = new Object();
+
+
+    this.alarmtypes = [ "Fire", "Hot", "Cold" ];
+    this.sensors = [ "Building 1, Room 21", "Building 2, Room 12" ];
+
+    this.toDoListTemplate.alarmtype = "";
+    this.toDoListTemplate.sensor = "";
+    this.toDoTemplates = []
+
 });
+
+function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+        $mdDialog.hide();
+    };
+    $scope.cancel = function() {
+        $mdDialog.cancel();
+    };
+}
